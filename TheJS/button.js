f11f109f2e310ba25db99a9a0e97a729c5835caf
RJB <113512137+1RJB@users.noreply.github.com>
2023-08-02 16:47:06 +0800

    Add files via upload

--- TheJS/button.js
+++ TheJS/button.js
@@ -26,20 +26,19 @@ else if (window.location.href == 'https://1rjb.github.io/MyGame/email.html') {
     });
 }
 
-else if (window.location.href == 'https://1rjb.github.io/MyGame/thanks.html') {
-    document.querySelector('.about').addEventListener('click', function() {
-        console.log('About me button clicked');
-        change('about.html');
-    });
-    document.querySelector('.mygame').addEventListener('click', function() {
-        console.log('My game button clicked');
-        change('index.html');
-    });
-    document.querySelector('.contactme').addEventListener('click', function() {
-        console.log('Contact Me button clicked');
-        change('email.html');
-    });
-};
+document.querySelector('.about').addEventListener('click', function() {
+    console.log('About me button clicked');
+    change('about.html');
+});
+document.querySelector('.mygame').addEventListener('click', function() {
+    console.log('My game button clicked');
+    change('index.html');
+});
+document.querySelector('.contactme').addEventListener('click', function() {
+    console.log('Contact Me button clicked');
+    change('email.html');
+});
+
 
 
     
