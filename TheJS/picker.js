c885eba3a21b53b237739342a28a3c27f3053d6e
RJB <113512137+1RJB@users.noreply.github.com>
2023-08-02 15:05:41 +0800

    Add files via upload

--- TheJS/picker.js
+++ TheJS/picker.js
@@ -9,6 +9,47 @@ for (var i = 1; i < 11; i++) {
         });
     })(i);
 };
+
+function change(a) {
+    window.location.assign(a);
+};
+
+if (window.location.href == 'https://1rjb.github.io/MyGame/index.html') {
+    document.querySelector('.contactme').addEventListener('click', function() {
+        console.log('Contact me button clicked');
+        change('email.html');
+    });
+    document.querySelector('.about').addEventListener('click', function() {
+        console.log('About me button clicked');
+        change('about.html');
+    });
+};
+
+if (window.location.href == 'https://1rjb.github.io/MyGame/about.html') {
+    document.querySelector('.contactme').addEventListener('click', function() {
+        console.log('Contact Me button clicked');
+        change('email.html');
+    });
+    document.querySelector('.mygame').addEventListener('click', function() {
+        console.log('My game button clicked');
+        change('index.html');
+    });
+    
+};
+
+
+if (window.location.href == 'https://1rjb.github.io/MyGame/email.html') {
+    document.querySelector('.about').addEventListener('click', function() {
+        console.log('About me button clicked');
+        change('about.html');
+    });
+    document.querySelector('.mygame').addEventListener('click', function() {
+        console.log('My game button clicked');
+        change('index.html');
+    });
+};
+
+
     
 
 
